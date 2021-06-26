17086eea697130a124b99de578c33980d5f6e4b6
Lucas Ribeiro <53449579+LucasRibeiroRJBR@users.noreply.github.com>
2021-06-26 00:17:08 -0300

    Criação banco / Estilo de janela

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 from PyQt5 import uic, QtWidgets
+import sqlite3
 
-app = 
+def gerar():
+    conn = sqlite3.connect('db/data.db')
+    c = conn.cursor()
+
+    tabela = tela.input_nom_tabela.text()
+    
+    sql = c.execute("SELECT * FROM NOMES;").fetchall()
+    r = ''
+    for i in sql:
+        if tela.cb_nome.isChecked():            
+            r += f"INSERT INTO {tabela}(ID,NOME) VALUES (0,'{i[1]}');\n"
+
+    tela.resul.append(r)
+
+app = QtWidgets.QApplication([])
+tela = uic.loadUi('gui/tela.ui')
+
+tela.bt_gerar.clicked.connect(gerar)
+
+tela.show()
+app.exec()
 
