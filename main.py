1935fd6fea9c464ca4717be1b7b21a1d35e4e84d
Lucas Ribeiro <53449579+LucasRibeiroRJBR@users.noreply.github.com>
2021-06-27 15:57:20 -0300

    Status

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 from PyQt5 import uic, QtWidgets
-import sqlite3
+import sqlite3, random
 
 def gerar():
     conn = sqlite3.connect('db/data.db')
     c = conn.cursor()
 
+    tela.resul.clear()
+
     tabela = tela.input_nom_tabela.text()
     
-    sql = c.execute("SELECT * FROM NOMES;").fetchall()
+    sql_nomes = c.execute("SELECT * FROM NOMES;").fetchall()
+    sql_sobrenomes = c.execute("SELECT * FROM SOBRENOMES;").fetchall()
+    sql_uf = c.execute("SELECT * FROM UF;").fetchall()
+
     r = ''
-    for i in sql:
-        if tela.cb_nome.isChecked():            
-            r += f"INSERT INTO {tabela}(ID,NOME) VALUES (0,'{i[1]}');\n"
+    for i_nomes,i_sobrenomes,i_ufs in zip(sql_nomes,sql_sobrenomes,sql_uf):
+        if tela.cb_nome.isChecked() and tela.cb_sobrenome.isChecked():
+            print(i_nomes,i_sobrenomes)            
+            r += f"INSERT INTO {tabela}(NOME,SOBRENOME) VALUES ('{random.choice(i_nomes)}','{random.choice(i_sobrenomes)}');\n"
+        elif tela.cb_nome.isChecked():            
+            r += f"INSERT INTO {tabela}(NOME) VALUES ('{i_nomes[1]}');\n"        
+        elif tela.cb_sobrenome.isChecked():            
+            r += f"INSERT INTO {tabela}(SOBRENOME) VALUES ('{i_sobrenomes[1]}');\n"
+        else:
+            pass
 
     tela.resul.append(r)
 
